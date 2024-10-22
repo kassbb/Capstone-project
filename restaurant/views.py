e9e454772eef868ffb337bd8a72ae93c16915bf6
kassbb <hkassambara01@gmail.com>
2024-10-22 23:50:10 +0000

    creation des API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics,viewsets
+from .models import Menu,Booking
+from .serializers import MenuSerializer,BookingSerializer
+from rest_framework import permissions
+# Vue pour la page d'accueil
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset=Booking.objects.all()
+    serializer_class=BookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
